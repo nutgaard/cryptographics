4e7d2e43c39e8928c700506f55d48fb70798ac40
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2019-06-26 21:21:18 +0200

    Updates

---
+++ precache-manifest.9cf289ba1cf830e7bbdedfd4e751cd53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f39a3152dd26fcbd9a504f455ad37a6",
+    "url": "/cryptographics/index.html"
+  },
+  {
+    "revision": "b635e4754a11a3e6adf3",
+    "url": "/cryptographics/static/css/main.3f24a661.chunk.css"
+  },
+  {
+    "revision": "bdd07d14ddcb4e05e1b8",
+    "url": "/cryptographics/static/js/2.e159d522.chunk.js"
+  },
+  {
+    "revision": "b635e4754a11a3e6adf3",
+    "url": "/cryptographics/static/js/main.1be1b61b.chunk.js"
+  },
+  {
+    "revision": "dc72a124c00cf3a98413",
+    "url": "/cryptographics/static/js/runtime~main.67830c15.js"
+  },
+  {
+    "revision": "cba90b7da541e8de103f44303f95cc2a",
+    "url": "/cryptographics/static/media/background.cba90b7d.jpg"
+  }
+]);
